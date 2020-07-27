f6c2a63beba339d9f9d4a5c729b07052c426bf67
esjastad <esjastad@gmail.com>
2020-07-27 12:35:52 -0700

    Added placeholder images for the drink mix game

--- react-ui/src/components/Game.js
+++ react-ui/src/components/Game.js
@@ -1,50 +1,50 @@
 import React, {Component} from 'react'; //Import component from react for the class to extend from.
 import { Grid, Row, Col } from 'react-flexbox-grid';
-
-
+import glass from './glass.png'
+import ingredient from './Ingredient.png'
 export class Game extends Component {
     render(){
         return (
             <Grid fluid>
               <Row>
-                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
-                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
-                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
-                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
-                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
-                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center"><input type="image" alt="Glass" src={glass}/></Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center"><input type="image" alt="Glass" src={glass}/></Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center"><input type="image" alt="Glass" src={glass}/></Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center"><input type="image" alt="Glass" src={glass}/></Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center"><input type="image" alt="Glass" src={glass}/></Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center"><input type="image" alt="Glass" src={glass}/></Col>
               </Row>
               <Row>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
               </Row>
               <Row>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
               </Row>
               <Row>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
               </Row>
               <Row>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
-                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center"><input type="image" alt="Ingredient" src={ingredient}/></Col>
               </Row>
             </Grid>
           );
