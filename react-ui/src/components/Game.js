f144e08989dd09436c5c2623a2ebbf3a8165b8bc
esjastad <esjastad@gmail.com>
2020-07-27 12:14:50 -0700

    Added initial drink mixing game layout design

--- react-ui/src/components/Game.js
+++ react-ui/src/components/Game.js
@@ -1,12 +1,53 @@
 import React, {Component} from 'react'; //Import component from react for the class to extend from.
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
 
 export class Game extends Component {
-
     render(){
-        return(
-            <div className="mt-d d-flex justify-content-center">
-                <h3>Welcome to the cocktail game! This is the game page!</h3>
-            </div>
-        )
+        return (
+            <Grid fluid>
+              <Row>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
+                <Col xs={1} md={2} className="bg-success border d-flex justify-content-center">GlassType</Col>
+              </Row>
+              <Row>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+              </Row>
+              <Row>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+              </Row>
+              <Row>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+              </Row>
+              <Row>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+                <Col xs={1} md={2} className="bg-primary border d-flex justify-content-center">IngredientType</Col>
+              </Row>
+            </Grid>
+          );
     }
+
 }
--- react-ui/src/components/Navigation.js
+++ react-ui/src/components/Navigation.js
@@ -7,15 +7,19 @@ export class Navigation extends Component {
         return(
             <Navbar bg="dark" expand="lg">
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav"/>
-
-                <Nav>
+                <Nav className="mt-d d-flex justify-content-center">
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/">Home</NavLink>
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/game">Game</NavLink>
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/drinks">Drinks</NavLink>
+                    
+                </Nav>
+                <Navbar.Collapse id="responsive-navbar-nav"/>
+                <Nav>
                     <NavLink className="d-inline p-2 bg-dark text-white" to="/about">About</NavLink>
                 </Nav>
             </Navbar>
         )
     }
 }
+
+
