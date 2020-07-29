ccb77ca8b99a1b5861ed19fad8abf920b4450688
esjastad <esjastad@gmail.com>
2020-07-29 13:16:03 -0700

    Populating drink ingredients with proper images and data, pushing for team analysis on how to access elements of the array

--- react-ui/src/components/Game.js
+++ react-ui/src/components/Game.js
@@ -9,12 +9,17 @@ function handleClick()
     console.log("HEY")
 }
 export class Game extends Component {
+  
     constructor(props) {
         super(props);
         this.state = {
           error: null,
           isLoaded: false,
-          items: []
+          items: [],
+          aIng: [],
+          oIng: [],
+          preIng: "https://www.thecocktaildb.com/images/ingredients/",
+          affIng: "-Small.png"
         };
       }
     
@@ -23,11 +28,11 @@ export class Game extends Component {
           .then(res => res.json())
           .then(
             (result) => {
-                console.log(result.drinks[0]);
               this.setState({
                 isLoaded: true,
                 items: result
               });
+              this.parseIng(result.drinks[0]);
             },
             
             (error) => {
@@ -38,7 +43,44 @@ export class Game extends Component {
             }
           )
       }
-    
+      
+      //This function parses the ingredients of a random get drink api call
+      //The ingredients are individually fetched and checked if they alcoholic or not
+      //The state arrays aIng and oIng will have ingredients pushed to them respectively
+      parseIng(result) {
+        for (var ing in result){
+          var temp = ing.split("strIngredient");
+          if (temp[0] === "" && result[ing] != null)
+          {
+            fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?i=" + result[ing])
+            .then(res => res.json())
+            .then(
+              (result) => {
+                if(result.ingredients[0].strAlcohol != null){
+                  this.state.aIng.push(result.ingredients[0].strIngredient);
+                }
+                else{
+                  this.state.oIng.push(result.ingredients[0].strIngredient);
+                }
+              },       
+              (error) => {
+                this.setState({
+                  isLoaded: true,
+                  error
+                });
+              }
+            )            
+          }
+           
+        }
+        console.log("Here is the non alcoholic ingredients I found: ");
+        console.log(this.state.oIng);
+        console.log("Here is the alcoholic ingredients I found: ");
+        console.log(this.state.aIng);
+        console.log("Here is an attempt to access the first index: ");
+        console.log(this.state.aIng[0]);
+      }
+
       render() {
         const { error, isLoaded, items } = this.state;
         if (error) {
@@ -58,10 +100,10 @@ export class Game extends Component {
                     <Col xs md={2} className="bg-success border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Glass" src={glass}/></Col>
                 </Row>
                 <Row>
-                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={alcohol}/></Col>
-                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={alcohol}/></Col>
-                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={alcohol}/></Col>
-                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={alcohol}/></Col>
+                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={this.state.preIng + items.drinks[0].strIngredient1 + this.state.affIng}/></Col>
+                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={this.state.preIng + items.drinks[0].strIngredient1 + this.state.affIng}/></Col>
+                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={this.state.preIng + items.drinks[0].strIngredient1 + this.state.affIng}/></Col>
+                    <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={this.state.preIng + items.drinks[0].strIngredient1 + this.state.affIng}/></Col>
                     <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={alcohol}/></Col>
                     <Col xs md={2} className="bg-primary border d-flex justify-content-center"><input id="gimg" onClick={handleClick} type="image" alt="Alcohol" src={alcohol}/></Col>
                 </Row>
@@ -75,7 +117,7 @@ export class Game extends Component {
                 </Row>
                 <br></br>
                 <div className="m-5 d-flex justify-content-center">
-                   <input id="dimg" type="image" alt="DrinkToMake" src={items.drinks[0].strDrinkThumb}/>
+                   <input id="dimg" className="border-1 border-dark" type="image" alt="DrinkToMake" src={items.drinks[0].strDrinkThumb}/>
                 </div>
                 <h3 className="d-flex justify-content-center">{items.drinks[0].strDrink}</h3>
             </Grid>
