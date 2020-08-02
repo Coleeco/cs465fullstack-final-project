e7db4694838b117859895d7584ec576c28f17fbf
esjastad <esjastad@gmail.com>
2020-08-02 13:38:41 -0700

    Another refactor and Async handling

--- react-ui/src/components/Game.js
+++ react-ui/src/components/Game.js
@@ -3,6 +3,7 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import glass from './glass.png'
 import ingredient from './Ingredient.png'
 import alcohol from './Alcohol.png'
+import goal from './Goal.png'
 
 class GameIngredient extends Component{
   constructor(props) {
@@ -11,20 +12,24 @@ class GameIngredient extends Component{
       isLoaded: false,
       clicked: false,
       name: props.name,
-      /*isAnswer: props.answer,
-      measure: props.measure,*/
+      isAnswer: false,
+      measureType: "",
+      measureAmount: 0
     }
   }
 
-  handleClick(data)
+  handleClick()
   {
-    console.log(data.state.name);
+    this.setState({
+      clicked: !this.state.clicked
+    });
+    console.log(this.props.name);
   }
 
   render() {
-    const { url, bg, name } = this.props;
+    const { url, bg, name } = this.props;  
     return (
-      <Col xs md={2} className={bg + " border d-flex justify-content-center"}><input id="gimg" onClick={() => this.handleClick(this)} type="image" alt={name} src={url}/></Col>
+      <Col xs md={2} className={bg + " border d-flex justify-content-center"}><input id="gimg" onClick={() => this.handleClick()} type="image" alt={name} src={url}/></Col>
     );
   }
 };
@@ -34,14 +39,16 @@ export class Game extends Component {
     super(props);
     this.state = {
       isLoaded: false,
-      items: [], //Goal drink to make data
+      items: [{strDrinkThumb: goal, strDrink: "Cocktail"}], //Goal drink to make data
+      goalAlcIng: [],
+      goalNonIng: [],
       aIng: [], //array of alcoholic ingredients
+      aUrl: [alcohol, alcohol, alcohol, alcohol, alcohol, alcohol], //array of url images for alcohol ingredients
+      oUrl: [ingredient, ingredient, ingredient, ingredient, ingredient, ingredient], //array of url images for non alcohol ingredients
       oIng: [], //array of non alocholic ingredients
-      extras: [],  //extra drink data
+      extras: [],  //extra drinks data
       preIng: "https://www.thecocktaildb.com/images/ingredients/",  //ingredient prefix address
       affIng: "-Small.png", //ingredient postfix address
-      aSize: 0,
-      oSize: 0,
       glassBG: "bg-success",
       alcBG: "bg-primary",
       nonBG: "bg-info"
@@ -49,11 +56,11 @@ export class Game extends Component {
   };
 
   //Render an ingredient game piece
-  renderIngredient(turl, tbg, tname){
+  renderIngredient(turl, tbg, tname ="test"){
     return <GameIngredient url = {turl} bg = {tbg} name = {tname}/>;
   };
 
-  parseIngredients(drink){
+  parseIngredients(drink, goal = false){
     for (var ingredient in drink){
       var stringTemp = ingredient.split("strIngredient");
       if (stringTemp[0] === "" && drink[ingredient] != null)
@@ -62,13 +69,17 @@ export class Game extends Component {
         .then(res => res.json())
         .then(
           (result) => {
-            if(result.ingredients[0].strAlcohol != null){
+            if(result.ingredients[0].strAlcohol != null) {
               this.state.aIng.push(result.ingredients[0].strIngredient);
-              this.setState({aSize: this.state.aSize + 1});
+              this.state.aUrl.push(this.state.preIng + result.ingredients[0].strIngredient + this.state.affIng);
+              if(goal)
+                this.state.goalAlcIng.push(result.ingredients[0].strIngredient);
             }
-            else{
+            else {
               this.state.oIng.push(result.ingredients[0].strIngredient);
-              this.setState({aSize: this.state.oSize + 1});
+              this.state.oUrl.push(this.state.preIng + result.ingredients[0].strIngredient + this.state.affIng);
+              if(goal)
+                this.state.goalNonIng.push(result.ingredients[0].strIngredient);
             }
           },       
           (error) => {
@@ -87,10 +98,9 @@ export class Game extends Component {
     .then(res => res.json())
     .then(
       (result) => {
-        this.setState({
-          items: result
-        });
-        this.parseIngredients(this.state.items.drinks[0]);
+        this.state.items.shift();
+        this.state.items.push(result.drinks[0]);
+        this.parseIngredients(this.state.items[0]);
       },
       (error) => {
         this.setState({
@@ -99,11 +109,11 @@ export class Game extends Component {
         });
       }
     )
-    setTimeout(this.populateExtras(), 1000);
+  this.populateExtras();
   }
 
   populateExtras(){
-    for(let i = 0; i < 7; ++i){
+    for(let i = 0; i < 9; ++i){
       fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       .then(res => res.json())
       .then(
@@ -119,80 +129,59 @@ export class Game extends Component {
             error
           });
         }
-      )
-      setTimeout(() => {this.setState({isLoaded: true}); }, 1000);
+      )  
     };
+    setTimeout(() => {this.shiftArray();}, 1500);
   };
 
+  shiftArray(){
+    for (let i = 0; i < 6; ++i){
+      this.state.aUrl.shift();
+      this.state.oUrl.shift();
+    }
+    this.setState({isLoaded: true});
+  }
+
+  handleClick(){
+    console.log("The current drink to make info is: ");
+    console.log(this.state.items[0]);
+  }
+
   render() {
-    const { error, isLoaded, items,aIng,oIng, preIng, affIng, glassBG, alcBG, nonBG, preLoaded } = this.state;
-    if(!isLoaded){
-      return (
-        <div>LOADING!</div>
-        /*<Grid fluid>
-          <br></br>
-          <Row>
-            {this.renderIngredient(glass, glassBG, "glass1")}
-            {this.renderIngredient(glass, glassBG, "glass2")}
-            {this.renderIngredient(glass, glassBG, "glass3")}
-            {this.renderIngredient(glass, glassBG, "glass4")}
-            {this.renderIngredient(glass, glassBG, "glass5")}
-            {this.renderIngredient(glass, glassBG, "glass6")}
-          </Row>
-          <Row>
-            {this.renderIngredient(alcohol, alcBG, "alcohol1")}
-            {this.renderIngredient(alcohol, alcBG, "alcohol2")}
-            {this.renderIngredient(alcohol, alcBG, "alcohol3")}
-            {this.renderIngredient(alcohol, alcBG, "alcohol4")}
-            {this.renderIngredient(alcohol, alcBG, "alcohol5")}
-            {this.renderIngredient(alcohol, alcBG, "alcohol6")}
-          </Row>
-          <Row>
-            {this.renderIngredient(ingredient, nonBG, "ingredient1")}
-            {this.renderIngredient(ingredient, nonBG, "ingredient2")}
-            {this.renderIngredient(ingredient, nonBG, "ingredient3")}
-            {this.renderIngredient(ingredient, nonBG, "ingredient4")}
-            {this.renderIngredient(ingredient, nonBG, "ingredient5")}
-            {this.renderIngredient(ingredient, nonBG, "ingredient6")}
-          </Row>
-        </Grid>        */
-      );
-    }
-    else {
-      return (
-        <Grid fluid>
-          <br></br>
-          <Row>
-            {this.renderIngredient(glass, glassBG, "glass1")}
-            {this.renderIngredient(glass, glassBG, "glass2")}
-            {this.renderIngredient(glass, glassBG, "glass3")}
-            {this.renderIngredient(glass, glassBG, "glass4")}
-            {this.renderIngredient(glass, glassBG, "glass5")}
-            {this.renderIngredient(glass, glassBG, "glass6")}
-          </Row>
-          <Row>
-            {this.renderIngredient(preIng + aIng[0] + affIng, alcBG, aIng[0])}
-            {this.renderIngredient(preIng + aIng[1] + affIng, alcBG, aIng[1])}
-            {this.renderIngredient(preIng + aIng[2] + affIng, alcBG, aIng[2])}
-            {this.renderIngredient(preIng + aIng[3] + affIng, alcBG, aIng[3])}
-            {this.renderIngredient(preIng + aIng[4] + affIng, alcBG, aIng[4])}
-            {this.renderIngredient(preIng + aIng[5] + affIng, alcBG, aIng[5])}
-          </Row>
-          <Row>
-            {this.renderIngredient(preIng + oIng[0] + affIng, nonBG, oIng[0])}
-            {this.renderIngredient(preIng + oIng[1] + affIng, nonBG, oIng[1])}
-            {this.renderIngredient(preIng + oIng[2] + affIng, nonBG, oIng[2])}
-            {this.renderIngredient(preIng + oIng[3] + affIng, nonBG, oIng[3])}
-            {this.renderIngredient(preIng + oIng[4] + affIng, nonBG, oIng[4])}
-            {this.renderIngredient(preIng + oIng[5] + affIng, nonBG, oIng[5])}
-          </Row>
-          <br></br>
-          <div className="m-5 d-flex justify-content-center">
-            <input id="dimg" className="border-1 border-dark" type="image" alt="DrinkToMake" src={items.drinks[0].strDrinkThumb}/>
-          </div>
-          <h3 className="d-flex justify-content-center">{items.drinks[0].strDrink}</h3>
-        </Grid>        
-      );
-    }
-  };
+    const { error, isLoaded, items, aIng, oIng, preIng, affIng, glassBG, alcBG, nonBG, preLoaded, aUrl, oUrl } = this.state;
+    return (
+      <Grid fluid>
+      <br></br>
+      <Row>
+        {this.renderIngredient(glass, glassBG, "glass1")}
+        {this.renderIngredient(glass, glassBG, "glass2")}
+        {this.renderIngredient(glass, glassBG, "glass3")}
+        {this.renderIngredient(glass, glassBG, "glass4")}
+        {this.renderIngredient(glass, glassBG, "glass5")}
+        {this.renderIngredient(glass, glassBG, "glass6")}
+      </Row>
+      <Row>
+        {this.renderIngredient(aUrl[0], alcBG, aIng[0])}
+        {this.renderIngredient(aUrl[1], alcBG, aIng[1])}
+        {this.renderIngredient(aUrl[2], alcBG, aIng[2])}
+        {this.renderIngredient(aUrl[3], alcBG, aIng[3])}
+        {this.renderIngredient(aUrl[4], alcBG, aIng[4])}
+        {this.renderIngredient(aUrl[5], alcBG, aIng[5])}
+      </Row>
+      <Row>
+        {this.renderIngredient(oUrl[0], nonBG, oIng[0])}
+        {this.renderIngredient(oUrl[1], nonBG, oIng[1])}
+        {this.renderIngredient(oUrl[2], nonBG, oIng[2])}
+        {this.renderIngredient(oUrl[3], nonBG, oIng[3])}
+        {this.renderIngredient(oUrl[4], nonBG, oIng[4])}
+        {this.renderIngredient(oUrl[5], nonBG, oIng[5])}
+      </Row>
+        <div className="m-5 d-flex justify-content-center">
+          <input id="dimg" className="border-1 border-dark" type="image" alt="DrinkToMake" src={items[0].strDrinkThumb}/>
+        </div>
+        <h3 className="d-flex justify-content-center">{items[0].strDrink}</h3>
+        <button onClick={() => this.handleClick()}> Submit </button>
+      </Grid>
+    );
+  }
 }
