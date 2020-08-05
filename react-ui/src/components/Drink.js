c9c30e88e070bfb1589931f7a95134bc9efbf0c9
Todd Graham <toddgraham121@gmail.com>
2020-08-04 20:04:09 -0700

    Formatting files with Prettier

--- react-ui/src/components/Drink.js
+++ react-ui/src/components/Drink.js
@@ -3,31 +3,31 @@ import "./Search.css";
 import { Card } from "react-bootstrap";
 
 function Drink(props) {
-  if (props.data === null) {
-    return <h1>No results found</h1>;
-  } else {
-    let element = props.data.map((item, index) => {
-      return (
-        <Card style={{ width: "32%" }}>
-          {/* TODO: Change style */}
-          <Card.Img
-            variant="top"
-            src={item.strDrinkThumb}
-            alt={item.strDrink}
-          />
-          <Card.Body>
-            <Card.Title>{item.strDrink}</Card.Title>
-            <Card.Text>Type: {item.strAlcoholic}</Card.Text>
-            <Card.Text>
-              Ingredients: {item.strIngredient1}, {item.strIngredient2},{" "}
-              {item.strIngredient3}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      );
-    });
-    return <div className="drinkContainer">{element}</div>;
-  }
+	if (props.data === null) {
+		return <h1>No results found</h1>;
+	} else {
+		let element = props.data.map((item, index) => {
+			return (
+				<Card style={{ width: "32%" }}>
+					{/* TODO: Change style */}
+					<Card.Img
+						variant="top"
+						src={item.strDrinkThumb}
+						alt={item.strDrink}
+					/>
+					<Card.Body>
+						<Card.Title>{item.strDrink}</Card.Title>
+						<Card.Text>Type: {item.strAlcoholic}</Card.Text>
+						<Card.Text>
+							Ingredients: {item.strIngredient1},{" "}
+							{item.strIngredient2}, {item.strIngredient3}
+						</Card.Text>
+					</Card.Body>
+				</Card>
+			);
+		});
+		return <div className="drinkContainer">{element}</div>;
+	}
 }
 
 export default Drink;
--- react-ui/src/components/Search.js
+++ react-ui/src/components/Search.js
@@ -1,76 +1,82 @@
-import React, {Component} from 'react'; //Import component from react for the class to extend from.
-import './Search.css';
-import Drink from './Drink.js';
+import React, { Component } from "react"; //Import component from react for the class to extend from.
+import "./Search.css";
+import Drink from "./Drink.js";
 
 export class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            drinks: [],
-            isLoaded: false,
-        }
+	constructor(props) {
+		super(props);
+		this.state = {
+			drinks: [],
+			isLoaded: false,
+		};
 
-        this.searchFunction = this.searchFunction.bind(this);
-        this.randomFunction = this.randomFunction.bind(this);
-    }
+		this.searchFunction = this.searchFunction.bind(this);
+		this.randomFunction = this.randomFunction.bind(this);
+	}
 
-    searchFunction(event) {
-        const searchValue = document.getElementById('searchbar').value;
+	searchFunction(event) {
+		const searchValue = document.getElementById("searchbar").value;
 
-        event.preventDefault();
+		event.preventDefault();
 
-        let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
+		let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
 
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                this.setState({
-                    drinks: data.drinks,
-                    isLoaded: true,
-                });
-            })
-            .catch((error) => console.log(error));
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({
+					drinks: data.drinks,
+					isLoaded: true,
+				});
+			})
+			.catch((error) => console.log(error));
 
-            event.preventDefault();
-    }
+		event.preventDefault();
+	}
 
-    randomFunction(event) {
-        let url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+	randomFunction(event) {
+		let url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                this.setState({
-                    drinks: data.drinks,
-                    isLoaded: true,
-                });
-            })
-            .catch((error) => console.log(error));
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({
+					drinks: data.drinks,
+					isLoaded: true,
+				});
+			})
+			.catch((error) => console.log(error));
 
-            event.preventDefault();
-    }
+		event.preventDefault();
+	}
 
-    render(){
-        const { drinks, isLoaded } = this.state;
+	render() {
+		const { drinks, isLoaded } = this.state;
 
-        return(
-            <React.Fragment>
-                <div className="mt-d d-flex justify-content-center">
-                    <form className="cocktail-search">
-                        <h3>Welcome to cocktail search!</h3>
-                        <div class="form-search">
-                            <input id="searchbar" placeholder="Search a cocktail by name" />
-                        </div>
-                        <div class="btnrow">
-                            <button onClick={this.searchFunction}>Search</button>
-                            <button onClick={this.randomFunction}>Random</button>
-                        </div>
-                    </form>
-                </div>
-                
-                <Drink data={drinks} />
-                
-            </React.Fragment>
-        )
-    }
+		return (
+			<React.Fragment>
+				<div className="mt-d d-flex justify-content-center">
+					<form className="cocktail-search">
+						<h3>Welcome to cocktail search!</h3>
+						<div class="form-search">
+							<input
+								id="searchbar"
+								placeholder="Search a cocktail by name"
+							/>
+						</div>
+						<div class="btnrow">
+							<button onClick={this.searchFunction}>
+								Search
+							</button>
+							<button onClick={this.randomFunction}>
+								Random
+							</button>
+						</div>
+					</form>
+				</div>
+
+				<Drink data={drinks} />
+			</React.Fragment>
+		);
+	}
 }
