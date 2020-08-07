85c10ba90fd6acdb25ad5c6a5c5d630bdfc08417
Todd Graham <toddgraham121@gmail.com>
2020-08-07 14:30:36 -0700

    Addition of content on homepage and Home.css

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -1,12 +1,53 @@
-import React, {Component} from 'react'; //Import component from react for the class to extend from.
+import React, { Component } from "react"; //Import component from react for the class to extend from.
+import "./Home.css";
 
 export class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			randDrink: [],
+		};
+	}
 
-    render(){
-        return(
-            <div className="mt-d d-flex justify-content-center">
-                <h3>Welcome to cocktail mastery! This is the home page!</h3>
-            </div>
-        )
-    }
+	componentDidMount() {
+		let url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({
+					randDrink: data.drinks[0],
+				});
+			})
+			.catch((error) => console.log(error));
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<h1 id="aboutH1">Welcome to Cocktail Mastery!</h1>
+				<div className="mt-d d-flex justify-content-center">
+					<div
+						class="card flex-row flex-wrap border-dark"
+						id="homeRandomCard"
+					>
+						<div class="card-header">
+							<img
+								id="homeRandomImg"
+								src={this.state.randDrink.strDrinkThumb}
+								alt={this.state.randDrink.strDrink}
+							/>
+						</div>
+						<div class="card-body">
+							<h3 class="card-title">
+								Drink of the Minute:{" "}
+								{this.state.randDrink.strDrink}
+							</h3>
+							<p class="card-text">Description</p>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
