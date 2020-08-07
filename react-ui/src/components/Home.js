84a334dc2dba57ff1e25f4f4a9c694df6e5b4de0
Todd Graham <toddgraham121@gmail.com>
2020-08-07 15:09:43 -0700

    Added ingredients and instructions to Home.js

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -7,6 +7,8 @@ export class Home extends Component {
 		this.state = {
 			randDrink: [],
 		};
+
+		this.parseIngMeasure = this.parseIngMeasure.bind(this);
 	}
 
 	componentDidMount() {
@@ -22,29 +24,64 @@ export class Home extends Component {
 			.catch((error) => console.log(error));
 	}
 
+	parseIngMeasure(drink) {
+		let listIngredients = [];
+		let listMeasures = [];
+		for (var ingredient in drink) {
+			var ingTemp = ingredient.split("strIngredient");
+			var measureTemp = ingredient.split("strMeasure");
+			if (ingTemp[0] === "" && drink[ingredient] != null) {
+				listIngredients.push(drink[ingredient]);
+			}
+			if (measureTemp[0] === "" && drink[ingredient] != null) {
+				listMeasures.push(drink[ingredient]);
+			}
+		}
+
+		let element = listIngredients.map((item, index) => {
+			return (
+				<li key={index}>
+					{item}: {listMeasures[index]}
+				</li>
+			);
+		});
+
+		return <ul>{element}</ul>;
+	}
+
 	render() {
 		return (
 			<React.Fragment>
 				<h1 id="aboutH1">Welcome to Cocktail Mastery!</h1>
 				<div className="mt-d d-flex justify-content-center">
 					<div
-						class="card flex-row flex-wrap border-dark"
+						className="card flex-row flex-wrap border-dark"
 						id="homeRandomCard"
 					>
-						<div class="card-header">
+						<div className="card-header">
 							<img
 								id="homeRandomImg"
 								src={this.state.randDrink.strDrinkThumb}
 								alt={this.state.randDrink.strDrink}
 							/>
 						</div>
-						<div class="card-body">
-							<h3 class="card-title">
+						<div className="card-body">
+							<h3 className="card-title">
 								Drink of the Minute:{" "}
 								{this.state.randDrink.strDrink}
 							</h3>
-							<p class="card-text">Description</p>
+							<p className="card-text">
+								<b>Type:</b> {this.state.randDrink.strAlcoholic}
+							</p>
+							<p className="card-text">
+								<b>Ingredients:</b>
+								{this.parseIngMeasure(this.state.randDrink)}
+							</p>
 						</div>
+						<p id="homeRandomInstructions">
+							<b>Instructions:</b>
+							<br /> {this.state.randDrink.strInstructions}
+						</p>
 					</div>
 				</div>
 			</React.Fragment>
