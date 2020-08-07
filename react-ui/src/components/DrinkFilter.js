7782005976f4ef06243e267fd95c623056ed2f5f
Todd Graham <toddgraham121@gmail.com>
2020-08-07 12:46:38 -0700

    Changing the card return on DrinkFilter.js

--- react-ui/src/components/DrinkFilter.js
+++ react-ui/src/components/DrinkFilter.js
@@ -4,52 +4,36 @@ import { Card } from "react-bootstrap";
 
 function DrinkFilter(props) {
 	let drink = [];
-	if (props.data.lenth === 0) {
+	if (props.data === null) {
 		return <h1 id="SearchEmpty">No results found</h1>;
-	} else if (props.data === null) {
+	} else if (props.data.length === 0) {
 		return <h1 id="SearchEmpty">No results found</h1>;
 	} else {
-		props.data.map((item) => {
+		let element = props.data.map((item, index) => {
 			let url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${item.idDrink}`;
 
 			fetch(url)
 				.then((response) => response.json())
 				.then((data) => {
-					drink.push(data);
+					return data.drinks;
+				})
+				.then((drinks) => {
+					drinks.forEach((item) => {
+						drink.push(item);
+					});
 				})
 				.catch((error) => console.log(error));
+			console.log(drink);
+
+			// return (
+			// 	<Card>
+			// 		<Card.Body>
+			// 			<Card.Title>{drink[index].strDrink}</Card.Title>
+			// 		</Card.Body>
+			// 	</Card>
+			// );
 		});
 
-		// console.log(drink);
-		console.log(Object.keys(drink));
-
-		let element = drink.map((item, index) => {
-			console.log("test");
-		});
-		// let element1 = drink.map((item1, index) => {
-		// 	console.log("test");
-		// 	console.log(item1.strDrink);
-
-		// return (
-		// 	<Card
-		// 		id="drinkCard"
-		// 		onClick={() => handleDrinkClick(item1)}
-		// 		key={index}
-		// 	>
-		// 		<Card.Img
-		// 			variant="top"
-		// 			src={item1.strDrinkThumb}
-		// 			alt={item1.strDrink}
-		// 		/>
-		// 		<Card.Body>
-		// 			<Card.Title>{item1.strDrink}</Card.Title>
-		// 			<Card.Text>Type: {item1.strAlcoholic}</Card.Text>
-		// 			<Card.Text>Category: {item1.strCategory}</Card.Text>
-		// 			{/* <Card.Text>Ingredients: {listIngredients}</Card.Text> */}
-		// 		</Card.Body>
-		// 	</Card>
-		// );
-		// });
 		return <div className="drinkContainer">{element}</div>;
 	}
 }
