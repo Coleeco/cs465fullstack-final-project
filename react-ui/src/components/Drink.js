da5ee33e713f30f977585def941883c25d616aed
Todd Graham <toddgraham121@gmail.com>
2020-08-08 11:41:42 -0700

    Added modals to click feature in search

--- react-ui/src/components/Drink.js
+++ react-ui/src/components/Drink.js
@@ -1,70 +1,70 @@
-import React from "react";
-import "./Search.css";
-import { Card } from "react-bootstrap";
+// import React from "react";
+// import "./Search.css";
+// import { Card } from "react-bootstrap";
 
-function Drink(props) {
-	if (props.data === null) {
-		return <h1 id="SearchEmpty">No results found</h1>;
-	} else {
-		let element = props.data.map((item, index) => {
-			let listIngredients = parseIng(item);
+// function Drink(props) {
+// 	if (props.data === null) {
+// 		return <h1 id="SearchEmpty">No results found</h1>;
+// 	} else {
+// 		let element = props.data.map((item, index) => {
+// 			let listIngredients = parseIng(item);
 
-			return (
-				<Card
-					id="drinkCard"
-					onClick={() => handleDrinkClick(item)}
-					key={index}
-				>
-					<Card.Img
-						variant="top"
-						src={item.strDrinkThumb}
-						alt={item.strDrink}
-					/>
-					<Card.Body>
-						<Card.Title>{item.strDrink}</Card.Title>
-						<Card.Text>Type: {item.strAlcoholic}</Card.Text>
-						<Card.Text>Category: {item.strCategory}</Card.Text>
-						<Card.Text>Ingredients: {listIngredients}</Card.Text>
-					</Card.Body>
-				</Card>
-			);
-		});
-		return <div className="drinkContainer">{element}</div>;
-	}
-}
+// 			return (
+// 				<Card
+// 					id="drinkCard"
+// 					onClick={() => handleDrinkClick(item)}
+// 					key={index}
+// 				>
+// 					<Card.Img
+// 						variant="top"
+// 						src={item.strDrinkThumb}
+// 						alt={item.strDrink}
+// 					/>
+// 					<Card.Body>
+// 						<Card.Title>{item.strDrink}</Card.Title>
+// 						<Card.Text>Type: {item.strAlcoholic}</Card.Text>
+// 						<Card.Text>Category: {item.strCategory}</Card.Text>
+// 						<Card.Text>Ingredients: {listIngredients}</Card.Text>
+// 					</Card.Body>
+// 				</Card>
+// 			);
+// 		});
+// 		return <div className="drinkContainer">{element}</div>;
+// 	}
+// }
 
-function parseIng(drink) {
-	let listIngredients = "";
-	for (var ingredient in drink) {
-		var stringTemp = ingredient.split("strIngredient");
-		if (stringTemp[0] === "" && drink[ingredient] != null) {
-			if (ingredient === "strIngredient1") {
-				listIngredients += drink[ingredient];
-			} else {
-				listIngredients += `, ${drink[ingredient]}`;
-			}
-		}
-	}
-	return listIngredients;
-}
+// function parseIng(drink) {
+// 	let listIngredients = "";
+// 	for (var ingredient in drink) {
+// 		var stringTemp = ingredient.split("strIngredient");
+// 		if (stringTemp[0] === "" && drink[ingredient] != null) {
+// 			if (ingredient === "strIngredient1") {
+// 				listIngredients += drink[ingredient];
+// 			} else {
+// 				listIngredients += `, ${drink[ingredient]}`;
+// 			}
+// 		}
+// 	}
+// 	return listIngredients;
+// }
 
-function handleDrinkClick(drink) {
-	console.log(drink.strDrink);
-	// let listIngredients = parseIng(drink);
-	return (
-		<Card id="clickedDrinkCard">
-			<Card.Img
-				variant="top"
-				src={drink.strDrinkThumb}
-				alt={drink.strDrink}
-			/>
-			<Card.Body>
-				<Card.Title>{drink.strDrink}</Card.Title>
-				<Card.Text>Type: {drink.strAlcoholic}</Card.Text>
-				<Card.Text>Category: {drink.strCategory}</Card.Text>
-			</Card.Body>
-		</Card>
-	);
-}
+// function handleDrinkClick(drink) {
+// 	console.log(drink.strDrink);
+// 	// let listIngredients = parseIng(drink);
+// 	return (
+// 		<Card id="clickedDrinkCard">
+// 			<Card.Img
+// 				variant="top"
+// 				src={drink.strDrinkThumb}
+// 				alt={drink.strDrink}
+// 			/>
+// 			<Card.Body>
+// 				<Card.Title>{drink.strDrink}</Card.Title>
+// 				<Card.Text>Type: {drink.strAlcoholic}</Card.Text>
+// 				<Card.Text>Category: {drink.strCategory}</Card.Text>
+// 			</Card.Body>
+// 		</Card>
+// 	);
+// }
 
-export default Drink;
+// export default Drink;
--- react-ui/src/components/Search.js
+++ react-ui/src/components/Search.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react"; //Import component from react for the class to extend from.
 import "./Search.css";
-import Drink from "./Drink.js";
-import DrinkFilter from "./DrinkFilter.js";
+// import Drink from "./Drink.js";
+import { Card, Modal, Button } from "react-bootstrap";
+// import DrinkFilter from "./DrinkFilter.js";
 import { AddFav } from "./Favorites";
 
 export class Search extends Component {
@@ -110,19 +111,311 @@ export class Search extends Component {
 				</div>
 
 				{/* Depending on the type of search (and the parameters returned from the API) we use a different Drink template */}
-				{this.state.select !== "name" && <DrinkFilter data={drinks} />}
-				{this.state.select === "name" && <Drink data={drinks} />}
+				{this.state.select !== "name" && (
+					<SearchDrinkFilterModal data={drinks} />
+				)}
+				{this.state.select === "name" && (
+					<SearchDrinkModal data={drinks} />
+				)}
 			</React.Fragment>
 		);
 	}
 }
 
-// class DrinkFilter extends Component {
-// 	constructor(props) {
-// 		super(props);
-// 	}
+export class SearchDrinkFilterModal extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalShow: false,
+			modalTitle: "",
+		};
 
-// 	render() {
-// 		return <div>test</div>;
-// 	}
-// }
+		this.modalShow = this.modalShow.bind(this);
+		this.modalClose = this.modalClose.bind(this);
+		this.modalUpdate = this.modalUpdate.bind(this);
+	}
+
+	modalShow() {
+		this.setState({
+			modalShow: true,
+		});
+	}
+
+	modalClose() {
+		this.setState({
+			modalShow: false,
+		});
+	}
+
+	modalUpdate(title) {
+		this.setState({
+			modalTitle: title,
+		});
+	}
+
+	render() {
+		return (
+			<div className="container mt-5">
+				<div className="row">
+					<div className="col">
+						<div className="card mx-auto">
+							<div className="card-body">
+								<Modal
+									show={this.state.modalShow}
+									onHide={this.modalClose}
+								>
+									<Modal.Header closeButton>
+										<Modal.Title>
+											{this.state.modalTitle}
+										</Modal.Title>
+									</Modal.Header>
+									<Modal.Body>
+										For more information, search{" "}
+										{this.state.modalTitle} by name.
+									</Modal.Body>
+									<Modal.Footer>
+										<Button
+											variant="primary"
+											onClick={this.modalClose}
+										>
+											Close
+										</Button>
+									</Modal.Footer>
+								</Modal>
+								<DrinkFilter
+									data={this.props.data}
+									showModal={this.modalShow}
+									modalUpdate={this.modalUpdate}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export class DrinkFilter extends Component {
+	constructor(props) {
+		super(props);
+
+		this.handleDrinkClick = this.handleDrinkClick.bind(this);
+	}
+
+	handleDrinkClick(item) {
+		this.props.modalUpdate(item.strDrink);
+		this.props.showModal();
+	}
+
+	formatCards() {
+		let element = this.props.data.map((item, index) => {
+			return (
+				<Card
+					id="drinkCard"
+					onClick={() => this.handleDrinkClick(item)}
+					key={index}
+				>
+					<Card.Img
+						variant="top"
+						src={item.strDrinkThumb}
+						alt={item.strDrink}
+					/>
+					<Card.Body>
+						<Card.Title>{item.strDrink}</Card.Title>
+					</Card.Body>
+				</Card>
+			);
+		});
+		return <div className="drinkContainer">{element}</div>;
+	}
+
+	render() {
+		return this.formatCards();
+	}
+}
+
+export class SearchDrinkModal extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalShow: false,
+			modalTitle: "",
+			modalIngredients: "",
+			modalType: "",
+			modalGlass: "",
+			modalCategory: "",
+			modalInstructions: "",
+		};
+
+		this.modalShow = this.modalShow.bind(this);
+		this.modalClose = this.modalClose.bind(this);
+		this.modalUpdate = this.modalUpdate.bind(this);
+	}
+
+	modalShow() {
+		this.setState({
+			modalShow: true,
+		});
+	}
+
+	modalClose() {
+		this.setState({
+			modalShow: false,
+		});
+	}
+
+	modalUpdate(title, type, category, glass, ingredients, instructions) {
+		this.setState({
+			modalTitle: title,
+			modalType: type,
+			modalCategory: category,
+			modalGlass: glass,
+			modalIngredients: ingredients,
+			modalInstructions: instructions,
+		});
+	}
+
+	render() {
+		return (
+			<div className="container mt-5">
+				<div className="row">
+					<div className="col">
+						<div className="card mx-auto">
+							<div className="card-body">
+								<Modal
+									show={this.state.modalShow}
+									onHide={this.modalClose}
+								>
+									<Modal.Header closeButton>
+										<Modal.Title>
+											{this.state.modalTitle}
+										</Modal.Title>
+									</Modal.Header>
+									<Modal.Body>
+										<b>Type:</b> {this.state.modalType}
+										<br />
+										<b>Glass:</b> {this.state.modalGlass}
+										<br />
+										<b>Category:</b>{" "}
+										{this.state.modalCategory}
+										<br />
+										<b>Ingredients:</b>{" "}
+										{this.state.modalIngredients}
+										<b>Instructions:</b>{" "}
+										{this.state.modalInstructions}
+										<br />
+									</Modal.Body>
+									<Modal.Footer>
+										<Button
+											variant="primary"
+											onClick={this.modalClose}
+										>
+											Close
+										</Button>
+									</Modal.Footer>
+								</Modal>
+								<Drink
+									data={this.props.data}
+									showModal={this.modalShow}
+									modalUpdate={this.modalUpdate}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export class Drink extends Component {
+	constructor(props) {
+		super(props);
+
+		this.handleDrinkClick = this.handleDrinkClick.bind(this);
+	}
+
+	handleDrinkClick(item) {
+		let ingredients = this.parseIngMeasure(item);
+		this.props.modalUpdate(
+			item.strDrink,
+			item.strAlcoholic,
+			item.strCategory,
+			item.strGlass,
+			ingredients,
+			item.strInstructions
+		);
+		this.props.showModal();
+	}
+
+	parseIng(drink) {
+		let listIngredients = "";
+		for (var ingredient in drink) {
+			var stringTemp = ingredient.split("strIngredient");
+			if (stringTemp[0] === "" && drink[ingredient] != null) {
+				if (ingredient === "strIngredient1") {
+					listIngredients += drink[ingredient];
+				} else {
+					listIngredients += `, ${drink[ingredient]}`;
+				}
+			}
+		}
+		return listIngredients;
+	}
+
+	parseIngMeasure(drink) {
+		let listIngredients = [];
+		let listMeasures = [];
+		for (var ingredient in drink) {
+			var ingTemp = ingredient.split("strIngredient");
+			var measureTemp = ingredient.split("strMeasure");
+			if (ingTemp[0] === "" && drink[ingredient] != null) {
+				listIngredients.push(drink[ingredient]);
+			}
+			if (measureTemp[0] === "" && drink[ingredient] != null) {
+				listMeasures.push(drink[ingredient]);
+			}
+		}
+
+		let element = listIngredients.map((item, index) => {
+			return (
+				<li key={index}>
+					{item}: {listMeasures[index]}
+				</li>
+			);
+		});
+
+		return <ul>{element}</ul>;
+	}
+
+	formatCards() {
+		let element = this.props.data.map((item, index) => {
+			let listIngredients = this.parseIng(item);
+			return (
+				<Card
+					id="drinkCard"
+					onClick={() => this.handleDrinkClick(item)}
+					key={index}
+				>
+					<Card.Img
+						variant="top"
+						src={item.strDrinkThumb}
+						alt={item.strDrink}
+					/>
+					<Card.Body>
+						<Card.Title>{item.strDrink}</Card.Title>
+						<Card.Text>Type: {item.strAlcoholic}</Card.Text>
+						<Card.Text>Category: {item.strCategory}</Card.Text>
+						<Card.Text>Ingredients: {listIngredients}</Card.Text>
+					</Card.Body>
+				</Card>
+			);
+		});
+		return <div className="drinkContainer">{element}</div>;
+	}
+
+	render() {
+		return this.formatCards();
+	}
+}
